ac477a4a1049b8fce3dc37123153ed8cd526bb28
mame0601 <anko6415@outlook.jp>
2021-04-07 16:53:26 +0900

    Add Follow Class

---
+++ src/models/Follow.java
@@ -0,0 +1,69 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "follows")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllFollows",
+        query = "SELECT f FROM Follow AS f ORDER BY f.id DESC"
+    ),
+    @NamedQuery(
+            name = "getFollowsCount",
+            query = "SELECT COUNT(f) FROM Follow AS f"
+    )
+})
+
+@Entity
+public class Follow
+{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name ="my_code", nullable = false)
+    private String my_code;
+
+    @Column(name = "follows_code", nullable = false, unique = true)
+    private String follows_code;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getMy_code()
+    {
+        return my_code;
+    }
+
+    public void setMy_code(String my_code)
+    {
+        this.my_code = my_code;
+    }
+
+    public String getFollows_code()
+    {
+        return follows_code;
+    }
+
+    public void setFollows_code(String follows_code)
+    {
+        this.follows_code = follows_code;
+    }
+
+
+}
